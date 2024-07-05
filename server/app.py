1097ae8aa92ad0600892510e75cd16a343b9353b
Guang <jouyang19@proton.me>
2024-07-05 00:44:28 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -17,8 +17,31 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        response_dict = [p.to_dict() for p in Plant.query.all()]
+        response = make_response(response_dict, 200)
+        return response
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name = data.get('name'),
+            image = data.get('image'),
+            price = data.get('price'),
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        response_dict = new_plant.to_dict()
+        response = make_response(response_dict, 201)
+        return response
+api.add_resource(Plants, '/plants')
 
+class PlantsByID(Resource):
+    def get(self, id):
+        response_dict = Plant.query.filter(Plant.id == id).first().to_dict()
+        response = make_response(response_dict, 200)
+        return response
+api.add_resource(PlantsByID, '/plants/<int:id>')
+    
 class PlantByID(Resource):
     pass
         
